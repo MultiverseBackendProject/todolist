a02692f1ac925b17ffbf5acee62fff217a1aeb08
Tani Cha <tanijcha@gmail.com>
2023-08-03 10:01:59 -0700

    added host to db.js

--- db.js
+++ db.js
@@ -2,8 +2,9 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize('database', 'username', 'password', {
     dialect: 'sqlite',
+    host: 'localhost',
+    storage: './db.sqlite',
     logging: false,
-    storage: './db.sqlite'
 });
 
 module.exports = { db }
