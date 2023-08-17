25e4e995dbdcc71bc114fadde3486adc86ad24f5
Tani Cha <tanijcha@gmail.com>
2023-08-17 13:27:07 -0700

    schema change, added association data for get task by id

---
+++ migrations/20230817200608-add-createdBy-to-todos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('todos', 'createdBy', {
+      type: Sequelize.STRING,
+      allowNull: true, // or false, depending on your requirements
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('todos', 'createdBy');
+  },
+};
+
+//this file exists because of updating our schema/db - had to add this type for work in assocation data/tier 5 reqs
--- models/Todo.js
+++ models/Todo.js
@@ -3,7 +3,8 @@ const { DataTypes } = require('sequelize');
 
 const Todo = db.define("todos", {
     task: DataTypes.STRING,
-    done: DataTypes.STRING
+    done: DataTypes.STRING,
+    createdBy: DataTypes.STRING
 });
 
 module.exports = { Todo };
