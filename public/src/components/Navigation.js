36e4458b276b228e84c5e924dabf4b4759e5043c
lekimberly17 <lekimberly509@gmail.com>
2023-11-02 10:29:45 -0700

    feat: nav bar styling

--- public/src/components/Navigation.js
+++ public/src/components/Navigation.js
@@ -5,30 +5,32 @@ import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import '../styles.css';
 
-const Navigation = () => {
+function Navigation() {
   return (
-    <Navbar sticky="top" bg="dark" variant="dark" className="mb-4 custom-navbar">
+    <Navbar sticky="top" bg="dark" variant="dark" className="custom-navbar">
       <Container>
-        <Navbar.Brand className="mr-auto">
-            <span className="brand-name font-weight-bold text-primary">To-Do List</span>
-        </Navbar.Brand>
-        <Nav className="ms-auto">
-          <Nav.Link as={NavLink} to="/callback/home" className="nav-link">
-            Homepage
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/callback/profile" className="nav-link">
-            Profile Page
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/callback/todo" className="nav-link">
-            To-Do List
-          </Nav.Link>
-          <Nav.Link as={NavLink} to="/callback/logout" className="nav-link">
-            Log Out
-          </Nav.Link>
-        </Nav>
+        <div className="d-flex justify-content-between w-100">
+          <Navbar.Brand className="mr-0">
+            <span>To-Do List</span>
+          </Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={NavLink} to="/callback/home" className="me-3">
+              Homepage
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/callback/profile" className="me-3">
+              Profile Page
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/callback/todo" className="me-3">
+              To-Do List
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/callback/logout">
+              Log Out
+            </Nav.Link>
+          </Nav>
+        </div>
       </Container>
     </Navbar>
   );
-};
+}
 
 export default Navigation;
